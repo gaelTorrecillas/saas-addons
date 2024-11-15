5063a2e468ccceab2ca94d9b065d9e055d4b9780
gaelTorrecillas <gael.torrecillas@mind-and-go.com>
2024-11-15 17:00:52 +0100

    [MIG] start mig

--- saas_cluster_simple/__manifest__.py
+++ saas_cluster_simple/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "SaaS: Simple cluster",
-    "version": "16.0.0.1.0",
+    "version": "18.0.0.1.0",
     "author": "IT-Projects LLC, Eugene Molotov",
     "support": "it@it-projects.info",
     "license": "LGPL-3",
