ed1aabd89b15d707eb185e3dba165defc10b201e
gael.torrecillas <gael.torrecillas@imerir.com>
2024-11-18 09:39:24 +0100

    Update __manifest__.py

--- saas_cluster_simple/__manifest__.py
+++ saas_cluster_simple/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "SaaS: Simple cluster",
-    "version": "18.0.0.1.0",
+    "version": "17.0.0.1.0",
     "author": "IT-Projects LLC, Eugene Molotov",
     "support": "it@it-projects.info",
     "license": "LGPL-3",
